abb016d622bc3b6b49bfde6ab04a5e1c859b8953
Silvio Colman <silviorafael.colman@gmail.com>
2023-04-05 16:38:27 -0400

    Initial Commit

---
+++ PageCurlSwipeAnimation/PageCurlSwipeAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PageCurlSwipeAnimationApp.swift
+//  PageCurlSwipeAnimation
+//
+//  Created by Silvio Colmán on 2023-04-05.
+//
+
+import SwiftUI
+
+@main
+struct PageCurlSwipeAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
